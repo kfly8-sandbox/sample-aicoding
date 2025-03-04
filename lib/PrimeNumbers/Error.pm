a776c5289e8f1b252cf8884b2c2ec4b472b97714
kobaken <kentafly88@gmail.com>
2025-03-04 11:26:58 +0900

    PODドキュメントの追加
    - PrimeNumbersモジュールにPODドキュメントを追加
    - 関数の詳細な説明と使用例を追加
    - アルゴリズムとパフォーマンス特性の説明を追加
    - PrimeNumbers::Errorモジュールにもドキュメントを追加

--- lib/PrimeNumbers/Error.pm
+++ lib/PrimeNumbers/Error.pm
@@ -3,7 +3,59 @@ use v5.40;
 use utf8;
 use experimental 'class';
 
+=head1 NAME
+
+PrimeNumbers::Error - PrimeNumbersモジュールで使用するエラークラス
+
+=head1 SYNOPSIS
+
+  use PrimeNumbers::Error;
+  
+  my $error = PrimeNumbers::Error::InvalidInput->new(
+      message => '入力は1以上の整数である必要があります'
+  );
+  
+  say $error->message;
+
+=head1 DESCRIPTION
+
+このモジュールは、PrimeNumbersモジュールで使用するエラークラスを提供します。
+エラータイプごとに専用のサブクラスを定義しています。
+
+=head1 CLASSES
+
+=head2 PrimeNumbers::Error
+
+基本エラークラス。全ての特定エラークラスの親クラスです。
+
+=head3 属性
+
+=over 4
+
+=item message
+
+エラーメッセージを格納します。読み取り専用です。
+
+=back
+
+=head2 PrimeNumbers::Error::InvalidInput
+
+無効な入力が与えられた場合に使用されるエラークラスです。
+PrimeNumbers::Errorを継承しています。
+
+=head1 AUTHOR
+
+開発者
+
+=head1 LICENSE
+
+指定なし
+
+=cut
+
 class PrimeNumbers::Error {
     field $message :param :reader;
 }
 
+class PrimeNumbers::Error::InvalidInput :isa(PrimeNumbers::Error) {
+}
